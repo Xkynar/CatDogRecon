45be846a8f992a66bde864f69b2424ae8a0bbaec
Xkynar <xkynar@gmail.com>
2015-12-19 11:14:04 +0000

    Initial commit

---
+++ CatDogRecon/Main.cpp
@@ -0,0 +1,20 @@
+#include <opencv\cv.h>
+#include <opencv2\core\core_c.h>
+#include <opencv2\highgui\highgui.hpp>
+#include <opencv2\imgproc\imgproc.hpp>
+#include <opencv2\features2d\features2d.hpp>
+#include <opencv2\nonfree\nonfree.hpp>
+#include <opencv2\nonfree\features2d.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <limits>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	cout << "meowoof";
+}
