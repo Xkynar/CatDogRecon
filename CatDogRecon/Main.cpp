bb3a11aecfea3d643082d8aaa94e9e99444b46a1
Xkynar <xkynar@gmail.com>
2015-12-23 10:30:12 +0000

    Paralelized testing and fixed surf hessian

--- CatDogRecon/Main.cpp
+++ CatDogRecon/Main.cpp
@@ -17,18 +17,31 @@
 using namespace cv;
 using namespace std;
 
+typedef tuple<int, int> Result;
+struct SortResults
+{
+	bool operator()(const Result& a, const Result& b)
+	{
+		if (get<0>(a) < get<0>(b))
+			return true;
+
+		return false;
+	}
+};
+
 const string ASSETS_PATH = "../Assets/";
+const string TRAIN_VOC_PATH = ASSETS_PATH + "vocabulary/";
 const string TRAIN_CAT_PATH = ASSETS_PATH + "cat/";
 const string TRAIN_DOG_PATH = ASSETS_PATH + "dog/";
 const string TEST_PATH = ASSETS_PATH + "test/";
 const int CAT_CLASS = 0;
 const int DOG_CLASS = 1;
-const int TRAIN_SIZE = 100; //12500
-const int TEST_SIZE = 100;
+const int TRAIN_SIZE = 500; //12500
+const int TEST_SIZE = 12500;
 
 //Vocabulary
 const int SURF_HESSIAN = 400;
-const int CLUSTER_COUNT = 100; //1000?
+const int CLUSTER_COUNT = 2000;
 
 //Storage
 const string VOCABULARY_PATH = ASSETS_PATH + "vocabulary.yml";
@@ -77,10 +90,13 @@ const string EXTRACTOR_TYPE = "SURF";
 Mat createVocabulary(vector<string> paths)
 {
 	//Extract train set descriptors and store them
-	SurfFeatureDetector detector(SURF_HESSIAN);
-	SurfDescriptorExtractor extractor;
+	Ptr<FeatureDetector> detector = FeatureDetector::create(DETECTOR_TYPE);
+	if (DETECTOR_TYPE == "SURF") detector->set("hessianThreshold", SURF_HESSIAN);
 
-	Mat trainDescriptors(1, extractor.descriptorSize(), extractor.descriptorType());
+	Ptr<DescriptorExtractor> extractor = DescriptorExtractor::create(EXTRACTOR_TYPE);
+	if (EXTRACTOR_TYPE == "SURF") extractor->set("hessianThreshold", SURF_HESSIAN);
+
+	Mat trainDescriptors(1, extractor->descriptorSize(), extractor->descriptorType());
 
 	//for each path, extract descriptors
 	for (int path = 0; path < paths.size(); path++)
@@ -91,12 +107,8 @@ Mat createVocabulary(vector<string> paths)
 			vector<KeyPoint> keypoints;
 			Mat descriptors;
 			Mat image = imread(paths[path] + to_string(i) + ".jpg", IMREAD_COLOR);
-			detector.detect(image, keypoints);
-			extractor.compute(image, keypoints, descriptors);
-			//Mat hue;
-			//drawKeypoints(image, keypoints, hue, Scalar::all(-1), DrawMatchesFlags::DEFAULT);
-			//imshow("hue" + i, hue);
-			//waitKey();
+			detector->detect(image, keypoints);
+			extractor->compute(image, keypoints, descriptors);
 
 			#pragma omp critical
 			{
@@ -108,6 +120,7 @@ Mat createVocabulary(vector<string> paths)
 	//Create a vocabulary from the stored descriptors
 	BOWKMeansTrainer bowTrainer(CLUSTER_COUNT); //clusters
 	bowTrainer.add(trainDescriptors);
+
 	Mat vocabulary = bowTrainer.cluster();
 
 	FileStorage fs(VOCABULARY_PATH, FileStorage::WRITE);
@@ -131,8 +144,12 @@ Mat loadVocabulary()
 void addDataClass(string classPath, int classId, Mat vocabulary, map<int, Mat> &data)
 {
 	Ptr<FeatureDetector> detector = FeatureDetector::create(DETECTOR_TYPE);
+	if (DETECTOR_TYPE == "SURF") detector->set("hessianThreshold", 100);
+
 	Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(MATCHER_TYPE);
+
 	Ptr<DescriptorExtractor> extractor = DescriptorExtractor::create(EXTRACTOR_TYPE);
+	if (EXTRACTOR_TYPE == "SURF") extractor->set("hessianThreshold", 100);
 
 	BOWImgDescriptorExtractor bowide(extractor, matcher);
 	bowide.setVocabulary(vocabulary);
@@ -213,17 +230,24 @@ Ptr<CvSVM> computeSVMClassifier(map<int, Mat> data)
 //Test methods
 void testBayesClassifier(Ptr<CvNormalBayesClassifier> classifier, Mat vocabulary)
 {
-	string results = "id,label\n";
+	set<Result, SortResults> sortedResults;
 
 	Ptr<FeatureDetector> detector = FeatureDetector::create(DETECTOR_TYPE);
+	if (DETECTOR_TYPE == "SURF") detector->set("hessianThreshold", SURF_HESSIAN);
+
 	Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(MATCHER_TYPE);
 	Ptr<DescriptorExtractor> extractor = DescriptorExtractor::create(EXTRACTOR_TYPE);
+	if (EXTRACTOR_TYPE == "SURF") extractor->set("hessianThreshold", SURF_HESSIAN);
 
 	BOWImgDescriptorExtractor bowide(extractor, matcher);
 	bowide.setVocabulary(vocabulary);
 
+	#pragma omp parallel for schedule(dynamic, 3)
 	for (int i = 1; i <= TEST_SIZE; i++)
 	{
+		if (i % 500 == 0)
+			cout << i << endl;
+
 		Mat image = imread(TEST_PATH + to_string(i) + ".jpg", IMREAD_COLOR);
 		vector<KeyPoint> kp;
 		Mat hst;
@@ -238,10 +262,24 @@ void testBayesClassifier(Ptr<CvNormalBayesClassifier> classifier, Mat vocabulary
 		}
 		else
 		{
-			classId = classifier->predict(hst);
+			classId = (int)classifier->predict(hst);
 		}
 
-		results += to_string(i) + "," + to_string(classId) + "\n";
+		Result r(i, classId);
+
+		#pragma omp critical
+		{
+			sortedResults.insert(r);
+		}
+	}
+
+	string results = "id,label\n";
+	set<Result>::iterator it;
+
+	for (it = sortedResults.begin(); it != sortedResults.end(); it++)
+	{
+		Result r = *it;
+		results += to_string(get<0>(r)) + "," + to_string(get<1>(r)) + "\n";
 	}
 
 	ofstream file(RESULTS_PATH);
@@ -250,18 +288,24 @@ void testBayesClassifier(Ptr<CvNormalBayesClassifier> classifier, Mat vocabulary
 
 void testSVMClassifier(Ptr<CvSVM> classifier, Mat vocabulary)
 {
-	string results = "id,label\n";
+	set<Result, SortResults> sortedResults;
 
 	Ptr<FeatureDetector> detector = FeatureDetector::create(DETECTOR_TYPE);
+	if (DETECTOR_TYPE == "SURF") detector->set("hessianThreshold", SURF_HESSIAN);
+
 	Ptr<DescriptorMatcher> matcher = DescriptorMatcher::create(MATCHER_TYPE);
 	Ptr<DescriptorExtractor> extractor = DescriptorExtractor::create(EXTRACTOR_TYPE);
+	if (EXTRACTOR_TYPE == "SURF") extractor->set("hessianThreshold", SURF_HESSIAN);
 
 	BOWImgDescriptorExtractor bowide(extractor, matcher);
 	bowide.setVocabulary(vocabulary);
 
+	#pragma omp parallel for schedule(dynamic, 3)
 	for (int i = 1; i <= TEST_SIZE; i++)
 	{
-		cout << i << endl;
+		if (i % 500 == 0)
+			cout << i << endl;
+
 		Mat image = imread(TEST_PATH + to_string(i) + ".jpg", IMREAD_COLOR);
 		vector<KeyPoint> kp;
 		Mat hst;
@@ -276,10 +320,24 @@ void testSVMClassifier(Ptr<CvSVM> classifier, Mat vocabulary)
 		}
 		else
 		{
-			classId = classifier->predict(hst);
+			classId = (int)classifier->predict(hst);
 		}
 
-		results += to_string(i) + "," + to_string(classId) + "\n";
+		Result r(i, classId);
+
+		#pragma omp critical
+		{
+			sortedResults.insert(r);
+		}
+	}
+
+	string results = "id,label\n";
+	set<Result>::iterator it;
+
+	for (it = sortedResults.begin(); it != sortedResults.end(); it++)
+	{
+		Result r = *it;
+		results += to_string(get<0>(r)) + "," + to_string(get<1>(r)) + "\n";
 	}
 
 	ofstream file(RESULTS_PATH);
@@ -288,15 +346,18 @@ void testSVMClassifier(Ptr<CvSVM> classifier, Mat vocabulary)
 
 int main()
 {
+	initModule_nonfree();
 	double t;
 
 	cout << "Creating vocabulary" << endl;
 	t = GetTickCount();
 	//Create a vocabulary from the whole dataset (cats AND dogs)
 	vector<string> datasetPaths;
-	datasetPaths.push_back(TRAIN_CAT_PATH);
-	datasetPaths.push_back(TRAIN_DOG_PATH);
+	datasetPaths.push_back(TRAIN_VOC_PATH);
+	//datasetPaths.push_back(TRAIN_CAT_PATH);
+	//datasetPaths.push_back(TRAIN_DOG_PATH);
 	Mat vocabulary = createVocabulary(datasetPaths);
+//	Mat vocabulary = loadVocabulary();
 	//----------------------------------------------------------
 	cout << GetTickCount() - t << "ms" << endl;
 
@@ -327,5 +388,8 @@ int main()
 
 	//gg
 	cout << "finish" << endl;
+	int a;
+	cin >> a;
+
 	return 0;
 }
